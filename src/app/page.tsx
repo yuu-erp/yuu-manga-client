5e6ccaf989e42e30171c8e173ff438645ee7a3da
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-05-29 20:48:38 +0700

    UPDATE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,4 @@
-// import React from "react";
-// import {
-//   dehydrate,
-//   HydrationBoundary,
-//   QueryClient,
-// } from "@tanstack/react-query";
-// import { getListManga } from "@/services";
-// import Mangas from "@/components/features/manga/Manga";
-
 import React from "react";
-
-// export default async function Manga() {
-//   const queryClient = new QueryClient();
-
-//   await queryClient.prefetchQuery({
-//     queryKey: ["mangas"],
-//     queryFn: getListManga,
-//   });
-
-//   return (
-//     <HydrationBoundary state={dehydrate(queryClient)}>
-//       <Mangas />
-//     </HydrationBoundary>
-//   );
-// }
-
 export default function Manga() {
   return <React.Fragment>Manga Home</React.Fragment>;
 }
--- src/components/partials/Sidebar.tsx
+++ src/components/partials/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../shared/Logo";
+import HomeIcon from "../icons/HomeIcon";
 
 const Sidebar = () => {
   return (
@@ -15,6 +16,19 @@ const Sidebar = () => {
           <div className="w-[275px] justify-between flex flex-col overflow-y-auto h-full px-2">
             <div className="flex flex-col items-start">
               <Logo className="my-2 max-w-full flex flex-col" />
+              <div className="mb-1 mt-[2px] w-full flex flex-col">
+                <nav className="flex flex-col items-start">
+                  <a className="flex flex-col items-start outline-none cursor-pointer grow w-full my-0">
+                    <div className="flex flex-row justify-center p-3 rounded-full hover:bg-slate-500">
+                      <HomeIcon className="w-7 h-7 text-[#e7e9ea]" />
+                      <div className="break-words max-w-full min-w-0 font-bold text-xl whitespace-nowrap text-[#e7e9ea] ml-5 mr-[26px] overflow-hidden">
+                        Home
+                      </div>
+                    </div>
+                  </a>
+                </nav>
+              </div>
+              <div className="w-[90%] my-1 flex flex-col">header1.2</div>
             </div>
             <div className="flex flex-col my-3">Header2</div>
           </div>
